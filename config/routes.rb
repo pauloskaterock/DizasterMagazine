902418088b8292124921f0a983d861012f4ef68f
Paulo Henrique dos Santos <paulo.hsantos2@sp.senac.br>
2024-10-07 17:54:14 -0300

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,11 @@ Rails.application.routes.draw do
 
   # # Defines the root path route ("/")
   # # root "posts#index"
+  #
+  # root "articles#index"
+
+  get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show"
 
   get "/articles", to: "articles#index"
 end
